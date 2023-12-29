b8aef0e27e80df9a355a13ced4cce4b66e51c23a
Geraldo Luiz <geraldobl58@gmail.com>
2023-12-29 09:03:56 -0300

    feat: Add model properties and seeds

--- src/scripts/seed.ts
+++ src/scripts/seed.ts
@@ -18,6 +18,54 @@ async function main() {
   } finally {
     await db.$disconnect();
   }
+
+  try {
+    await db.bathroom.createMany({
+      data: [
+        { quantity: 1 },
+        { quantity: 2 },
+        { quantity: 3 },
+        { quantity: 4 },
+        { quantity: 5 },
+      ],
+    });
+  } catch (error) {
+    console.error("Error seeding default type of bathroom", error);
+  } finally {
+    await db.$disconnect();
+  }
+
+  try {
+    await db.bedroom.createMany({
+      data: [
+        { quantity: 1 },
+        { quantity: 2 },
+        { quantity: 3 },
+        { quantity: 4 },
+        { quantity: 5 },
+      ],
+    });
+  } catch (error) {
+    console.error("Error seeding default type of bedroom", error);
+  } finally {
+    await db.$disconnect();
+  }
+
+  try {
+    await db.garage.createMany({
+      data: [
+        { quantity: 1 },
+        { quantity: 2 },
+        { quantity: 3 },
+        { quantity: 4 },
+        { quantity: 5 },
+      ],
+    });
+  } catch (error) {
+    console.error("Error seeding default type of garage", error);
+  } finally {
+    await db.$disconnect();
+  }
 }
 
 main();
