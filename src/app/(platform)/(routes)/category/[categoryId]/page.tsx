ae2382f051a9a34d1be000b878071dc4e69d375b
Geraldo Luiz <geraldobl58@gmail.com>
2024-01-03 19:20:16 -0300

    feat: Add page category id

--- src/app/(platform)/(routes)/category/[categoryId]/page.tsx
+++ src/app/(platform)/(routes)/category/[categoryId]/page.tsx
@@ -9,23 +9,17 @@ const CategoryPage = async () => {
   const categories = await prismadb.category.findMany();
 
   return (
-    <>
-      <Navbar />
-
-      <div className="mx-auto w-full max-w-[1600px] gap-5 p-8">
-        <div className="flex gap-4">
-          <div className="bg-zinc-100 w-[500px] p-6 rounded-lg">
-            <h3 className="text-lg">Busque seu imóvel</h3>
-            <div className="relative mt-4">
-              <SearchInput />
-            </div>
-          </div>
-          <div className="flex-1 bg-zinc-100 h-full p-6 rounded-lg">
-            <Categories data={categories} />
-          </div>
+    <div className="flex gap-4">
+      <div className="bg-zinc-100 w-[500px] p-6 rounded-lg">
+        <h3 className="text-lg">Busque seu imóvel</h3>
+        <div className="relative mt-4">
+          <SearchInput />
         </div>
       </div>
-    </>
+      <div className="flex-1 bg-zinc-100 h-full p-6 rounded-lg">
+        <Categories data={categories} />
+      </div>
+    </div>
   );
 };
 
