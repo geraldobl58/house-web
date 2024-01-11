ac7970e9b46bf49f491a2535ce8361fe009a4fa9
Geraldo Luiz <geraldobl58@gmail.com>
2024-01-11 18:56:07 -0300

    feat: Add component properties card

---
+++ src/app/(platform)/(public)/properties/[propertiesId]/components/properties-card.tsx
@@ -0,0 +1,77 @@
+import Link from "next/link";
+
+import { Bath, Bed, CarFront } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+
+import formatPrice from "@/lib/utils";
+
+interface PropertiesCardProps {
+  id: string;
+  name: string;
+  address: string;
+  neighborhood: string;
+  description: string;
+  garage: number;
+  bathroom: number;
+  bedroom: number;
+  price: number;
+}
+
+export const PropertiesCard = ({
+  id,
+  name,
+  address,
+  neighborhood,
+  description,
+  garage,
+  bathroom,
+  bedroom,
+  price,
+}: PropertiesCardProps) => {
+  return (
+    <div className="ml-10 p-4 w-[450px] space-y-4">
+      <h3 className="font-medium text-2xl text-primary">
+        <Link
+          href={`/properties/${id}`}
+          target="_blank"
+          className="hover:underline"
+        >
+          {name}
+        </Link>
+      </h3>
+      <p className="text-sm">{address}</p>
+      <p className="text-sm text-muted-foreground">{neighborhood}</p>
+      <p className="text-sm text-gray-700">{description}</p>
+
+      <div className="grid grid-cols-3 gap-2">
+        <div className="flex items-center">
+          <CarFront className="text-muted-foreground" />
+          <span className="ml-2 text-xs text-muted-foreground">
+            {garage} Garagem
+          </span>
+        </div>
+        <div className="flex items-center">
+          <Bath className="text-muted-foreground" />{" "}
+          <span className="ml-2 text-xs text-muted-foreground">
+            {bathroom} Banheiro
+          </span>
+        </div>
+        <div className="flex items-center">
+          <Bed className="text-muted-foreground" />{" "}
+          <span className="ml-2 text-xs text-muted-foreground">
+            {bedroom} Quartos
+          </span>
+        </div>
+      </div>
+
+      <p className="text-lg text-primary">{formatPrice(price)}</p>
+
+      <div>
+        <Link href={`/properties/${id}`} target="_blank">
+          <Button>Saiba mais</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
